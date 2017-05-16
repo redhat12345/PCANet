2043f1e508e14273164329dd52d99374720ff731
Peter Mitrano <mitranopeter@gmail.com>
2017-05-15 23:27:44 -0400

    extracted data loading so I can try mnist
    tensorboard log-dir:
    
    tensorboard log-dir:

--- pcanet.py
+++ pcanet.py
@@ -4,23 +4,15 @@
 # https://arxiv.org/pdf/1404.3606.pdf
 
 import tensorflow as tf
-import numpy as np
 from datetime import datetime
+from dataset_loader import load_cifar10, DatasetInfo
 import os
 import sys
 from subprocess import call
 
 
-# Global constants describing the CIFAR-10 data set.
-N_CHANNELS = 3
-IMAGE_SIZE = 32
-img_dim = IMAGE_SIZE * IMAGE_SIZE * N_CHANNELS
-NUM_CLASSES = 10
-NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
-
-
 class PCANet:
-    def __init__(self, image_batch, label_batch, global_step):
+    def __init__(self, image_batch, label_batch, info):
         self.image_batch = image_batch
         self.label_batch = label_batch
 
@@ -28,51 +20,13 @@ class PCANet:
         k2 = 5
         l1 = 10
         self.patches = tf.extract_image_patches(image_batch, ksizes=[1, k1, k2, 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding='SAME', name='x')
-        self.patches = tf.reshape(self.patches, [-1, k1 * k2, N_CHANNELS])
+        self.patches = tf.reshape(self.patches, [-1, k1 * k2, info.N_CHANNELS])
         self.zero_mean_patches = self.patches - tf.reduce_mean(self.patches, axis=2, keep_dims=True)
         x = tf.transpose(self.zero_mean_patches, [2, 1, 0])
         x_trans = tf.transpose(self.zero_mean_patches, [2, 0, 1])
         self.patches_covariance = tf.matmul(x, x_trans, name='patch_covariance')
         _, self.x_eig = tf.self_adjoint_eig(self.patches_covariance, name='x_eig')
         self.top_x_eig = tf.transpose(tf.reshape(self.x_eig[:, 0:l1], [3, k1, k2, l1]), [3, 2, 1, 0])
-
-
-def generate_image_and_label_batch(images, labels, min_queue_examples, batch_size):
-    with tf.name_scope("make_batches"):
-        num_preprocess_threads = 2
-        images, label_batch = tf.train.shuffle_batch(
-            [images, labels],
-            batch_size=batch_size,
-            num_threads=num_preprocess_threads,
-            capacity=min_queue_examples + 3 * batch_size,
-            min_after_dequeue=min_queue_examples)
-
-        tf.summary.image('images', images)
-
-        return images, tf.one_hot(tf.squeeze(label_batch), depth=NUM_CLASSES)
-
-
-def input_queue(filenames):
-    queue = tf.train.string_input_producer(filenames)
-    raw_img_dim = IMAGE_SIZE * IMAGE_SIZE * 3
-
-    with tf.name_scope("read"):
-        reader = tf.FixedLengthRecordReader(record_bytes=raw_img_dim + 1, name='input_reader')
-        _, record_str = reader.read(queue, name='read_op')
-        record_raw = tf.decode_raw(record_str, tf.uint8, name='decode_raw')
-
-        label = tf.cast(tf.slice(record_raw, [0], [1]), tf.int32)
-        image = tf.reshape(tf.slice(record_raw, [1], [raw_img_dim]), [3, 32, 32])
-        float_image = tf.cast(tf.transpose(image, [1, 2, 0]), tf.float32)
-
-    with tf.name_scope('preprocess'):
-        norm_image = tf.divide(float_image, 255.0, name='norm_images')
-        # gray_image = tf.reduce_sum(tf.multiply(norm_image, tf.constant([0.2126, 0.7512, 0.0722])), axis=2, name='grayscale')
-        # gray_image = tf.expand_dims(gray_image, axis=2)
-        # final_image = gray_image
-        final_image = norm_image
-
-    return final_image, label
 
 
 def main():
@@ -91,26 +45,14 @@ def main():
         call(cmd)
 
     # setup the input data pipelines
-    data_dir = 'cifar'
-    train_filenames = [os.path.join(data_dir, 'data_batch_%i.bin' % i) for i in range(1, 6)]
-    test_filenames = [os.path.join(data_dir, 'test_batch.bin')]
-
-    train_images, train_labels = input_queue(train_filenames)
-    test_images, test_labels = input_queue(test_filenames)
-
-    min_fraction_of_examples_in_queue = 0.4
-    min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN * min_fraction_of_examples_in_queue)
-    train_image_batch, train_label_batch = generate_image_and_label_batch(train_images, train_labels, min_queue_examples, 128)
-    test_image_batch, test_label_batch = generate_image_and_label_batch(test_images, test_labels, min_queue_examples, 10000)
+    train_image_batch, train_label_batch, test_image_batch, test_label_batch, info = load_cifar10()
 
     sess = tf.Session()
 
     tf.train.start_queue_runners(sess=sess)
 
-    global_step = tf.Variable(0, trainable=False, name='global_step')
-
     # define the model
-    m = PCANet(train_image_batch, train_label_batch, global_step)
+    m = PCANet(train_image_batch, train_label_batch, info)
 
     e = sess.run(m.top_x_eig)
 
@@ -118,7 +60,7 @@ def main():
     plt.figure()
     plt.imshow(e[0], interpolation='none')
     plt.show()
-#
+
 
 if __name__ == '__main__':
     main()
