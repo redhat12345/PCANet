dbb8a532abec625284d921252d7c38bb031e37ac
Peter Mitrano <mitranopeter@gmail.com>
2017-05-17 16:31:41 -0400

    running with 2 layer dense classifier??
    tensorboard log-dir: log_data/May_17/May_17_16:31:41/

--- pcanet.py
+++ pcanet.py
@@ -25,9 +25,6 @@ class PCANet:
         with tf.name_scope("extract_patches1"):
             self.patches1 = tf.extract_image_patches(image_batch, [1, k1, k2, 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding='SAME', name='patches')
             self.patches1 = tf.reshape(self.patches1, [-1, k1 * k2, info.N_CHANNELS], name='patches_shaped')
-            # TODO: figure out how to unvectorize for multi-channel images
-            # self.patches1 = tf.reshape(self.patches1, [-1, info.N_CHANNELS,  k1 * k2], name='patches_shaped')
-            # self.patches1 = tf.transpose(self.patches1, [0, 2, 1])
             self.zero_mean_patches1 = self.patches1 - tf.reduce_mean(self.patches1, axis=1, keep_dims=True, name='patch_means')
             x1 = tf.transpose(self.zero_mean_patches1, [2, 1, 0])
             x1_trans = tf.transpose(self.zero_mean_patches1, [2, 0, 1])
@@ -43,47 +40,54 @@ class PCANet:
 
         with tf.name_scope("convolution1"):
             self.conv1 = tf.nn.conv2d(image_batch, self.top_x_eig1, strides=[1, 1, 1, 1], padding='SAME')
-            self.conv1 = tf.transpose(self.conv1, [0, 3, 1, 2])
-            self.conv1_batch = tf.reshape(self.conv1, [-1, info.IMAGE_W, info.IMAGE_H, 1])
+            self.conv1_batch = tf.reshape(tf.transpose(self.conv1, [0, 3, 1, 2]), [-1, info.IMAGE_W, info.IMAGE_H, 1])
 
             tf.summary.image('conv1', self.conv1_batch, max_outputs=l1)
 
-        with tf.name_scope("extract_patches2"):
-            self.patches2 = tf.extract_image_patches(self.conv1_batch, [1, k1, k2, 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding='SAME', name='patches')
-            self.patches2 = tf.expand_dims(tf.reshape(self.patches2, [-1, k1 * k2], name='patches_shaped'), axis=2)
-            self.zero_mean_patches2 = self.patches2 - tf.reduce_mean(self.patches2, axis=1, keep_dims=True, name='patch_means')
-            x2 = tf.transpose(self.zero_mean_patches2, [2, 1, 0])
-            x2_trans = tf.transpose(self.zero_mean_patches2, [2, 0, 1])
-            self.patches_covariance2 = tf.matmul(x2, x2_trans, name='patch_covariance')
+        # with tf.name_scope("extract_patches2"):
+        #     self.patches2 = tf.extract_image_patches(self.conv1_batch, [1, k1, k2, 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding='SAME', name='patches')
+        #     self.patches2 = tf.expand_dims(tf.reshape(self.patches2, [-1, k1 * k2], name='patches_shaped'), axis=2)
+        #     self.zero_mean_patches2 = self.patches2 - tf.reduce_mean(self.patches2, axis=1, keep_dims=True, name='patch_means')
+        #     x2 = tf.transpose(self.zero_mean_patches2, [2, 1, 0])
+        #     x2_trans = tf.transpose(self.zero_mean_patches2, [2, 0, 1])
+        #     self.patches_covariance2 = tf.matmul(x2, x2_trans, name='patch_covariance')
+        #
+        # with tf.name_scope("eignvalue_decomposition2"):
+        #     self.x_eig_vals2, self.x_eig2 = tf.self_adjoint_eig(self.patches_covariance2, name='x_eig')
+        #     self.top_x_eig2 = tf.reverse(self.x_eig2, axis=[2])[:, :, 0:l2]
+        #     self.top_x_eig2 = tf.transpose(tf.reshape(self.top_x_eig2, [1, k1, k2, l2]), [2, 1, 0, 3])
+        #
+        #     self.filt2_viz = tf.transpose(self.top_x_eig2, [3, 0, 1, 2])
+        #     tf.summary.image('filt2', self.filt2_viz, max_outputs=l2)
 
-        with tf.name_scope("eignvalue_decomposition2"):
-            self.x_eig_vals2, self.x_eig2 = tf.self_adjoint_eig(self.patches_covariance2, name='x_eig')
-            self.top_x_eig2 = tf.reverse(self.x_eig2, axis=[2])[:, :, 0:l2]
-            self.top_x_eig2 = tf.transpose(tf.reshape(self.top_x_eig2, [1, k1, k2, l2]), [2, 1, 0, 3])
+        # with tf.name_scope("convolution2"):
+        #     self.conv2 = tf.nn.conv2d(self.conv1_batch, self.top_x_eig2, strides=[1, 1, 1, 1], padding='SAME')
+        #     self.conv2 = tf.reshape(tf.transpose(self.conv2, [0, 3, 1, 2]), [-1, l1, l2, info.IMAGE_W, info.IMAGE_H])
+        #     self.conv2_batch = tf.reshape(self.conv2, [-1, info.IMAGE_W, info.IMAGE_H, 1])
+        #     tf.summary.image('conv2', self.conv2_batch, max_outputs=l2)
 
-            self.filt2_viz = tf.transpose(self.top_x_eig2, [3, 0, 1, 2])
-            tf.summary.image('filt2', self.filt2_viz, max_outputs=l2)
+        with tf.name_scope("h1"):
+            self.h1_dim = 128
+            self.w_fc1 = tf.Variable(tf.truncated_normal([info.IMAGE_W * info.IMAGE_H * l1,  self.h1_dim], 0, 0.1), name='w_fc1')
+            self.b_fc1 = tf.Variable(tf.constant(0.1, shape=[self.h1_dim]), name='b_fc1')
+            self.conv1_flat = tf.reshape(self.conv1, [-1, info.IMAGE_W * info.IMAGE_H * l1])
+            self.h1 = tf.nn.relu(self.conv1_flat @ self.w_fc1 + self.b_fc1, name='h1')
 
-        with tf.name_scope("convolution2"):
-            self.conv2 = tf.nn.conv2d(self.conv1_batch, self.top_x_eig2, strides=[1, 1, 1, 1], padding='SAME')
-            self.conv2 = tf.reshape(tf.transpose(self.conv2, [0, 3, 1, 2]), [-1, l1, l2, info.IMAGE_W, info.IMAGE_H])
-            self.conv2_batch = tf.reshape(self.conv2, [-1, info.IMAGE_W, info.IMAGE_H, 1])
-            tf.summary.image('conv2', self.conv2_batch, max_outputs=l2)
+        with tf.name_scope("h2"):
+            self.h2_dim = info.NUM_CLASSES
+            self.w_fc2 = tf.Variable(tf.truncated_normal([self.h1_dim, self.h2_dim], 0, 0.1), name='w_fc2')
+            self.b_fc2 = tf.Variable(tf.constant(0.1, shape=[self.h2_dim]), name='b_fc2')
+            self.h2 = tf.nn.relu(self.h1 @ self.w_fc2 + self.b_fc2, name='h2')
 
-        with tf.name_scope("binary_quantize"):
-            self.binary_quantize = tf.cast(self.conv2 > 0, tf.float32)
-            self.powers_of_two = tf.constant([2 ** n for n in range(0, l2)], dtype=tf.float32)
-            self.binary_encoded = tf.reduce_sum(tf.transpose(self.binary_quantize, [0, 1, 3, 4, 2]) * self.powers_of_two, axis=4)
-            print(self.binary_encoded.get_shape())
+        with tf.name_scope("train"):
+            self.label_batch_one_hot = tf.one_hot(tf.cast(label_batch, tf.int32), depth=info.NUM_CLASSES)
+            self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.h2, labels=self.label_batch_one_hot)
+            self.train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+            self.predictions = tf.cast(tf.argmax(tf.nn.softmax(self.h2)), tf.float32)
+            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.label_batch_one_hot, self.predictions), tf.float32), name='accuracy')
 
-            self.binary_quantize_viz = tf.reshape(tf.expand_dims(self.binary_quantize, axis=4), [-1, info.IMAGE_W, info.IMAGE_H, 1])
-            self.binary_encoded_viz = tf.expand_dims(self.binary_encoded[:, 1, :, :], axis=3)
-            tf.summary.image('quantized', self.binary_quantize_viz, max_outputs=10)
-            tf.summary.image('encoded', self.binary_encoded_viz, max_outputs=10)
-
-        with tf.name_scope("histograms"):
-            tf.extract_image_patches(self.binary_encoded, [1, B, B, 1], [1, 4, 4, ], [1, 1, 1, 1], padding='NONE')
-
+            tf.summary.scalar("loss", self.loss)
+            tf.summary.scalar("accuracy", self.accuracy)
 
 
 def main():
@@ -124,8 +128,10 @@ def main():
     writer.add_graph(sess.graph)
     merged_summary = tf.summary.merge_all()
 
-    _, summary, b = sess.run([m.conv2_batch, merged_summary, m.binary_encoded])
-    writer.add_summary(summary, 0)
+    for i in range(10):
+        _, summary, l = sess.run([m.train, merged_summary, m.loss])
+        print(l, i)
+        writer.add_summary(summary, 0)
 
     writer.close()
 
