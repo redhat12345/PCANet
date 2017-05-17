f61042b6b9820b6dd197a3584406b5f4ac4fb245
Peter Mitrano <mitranopeter@gmail.com>
2017-05-17 16:47:26 -0400

    two layers doesn't do better
    I'm probably doing it wrong anyways
    
    tensorboard log-dir: log_data/May_17/May_17_16:41:53

--- pcanet.py
+++ pcanet.py
@@ -40,38 +40,39 @@ class PCANet:
 
         with tf.name_scope("convolution1"):
             self.conv1 = tf.nn.conv2d(image_batch, self.top_x_eig1, strides=[1, 1, 1, 1], padding='SAME')
-            self.conv1_batch = tf.reshape(tf.transpose(self.conv1, [0, 3, 1, 2]), [-1, info.IMAGE_W, info.IMAGE_H, 1])
+            self.conv1_t = tf.transpose(self.conv1, [0, 3, 1, 2])
+            self.conv1_batch = tf.reshape(self.conv1_t, [-1, info.IMAGE_W, info.IMAGE_H, 1])
 
             tf.summary.image('conv1', self.conv1_batch, max_outputs=l1)
 
-        # with tf.name_scope("extract_patches2"):
-        #     self.patches2 = tf.extract_image_patches(self.conv1_batch, [1, k1, k2, 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding='SAME', name='patches')
-        #     self.patches2 = tf.expand_dims(tf.reshape(self.patches2, [-1, k1 * k2], name='patches_shaped'), axis=2)
-        #     self.zero_mean_patches2 = self.patches2 - tf.reduce_mean(self.patches2, axis=1, keep_dims=True, name='patch_means')
-        #     x2 = tf.transpose(self.zero_mean_patches2, [2, 1, 0])
-        #     x2_trans = tf.transpose(self.zero_mean_patches2, [2, 0, 1])
-        #     self.patches_covariance2 = tf.matmul(x2, x2_trans, name='patch_covariance')
-        #
-        # with tf.name_scope("eignvalue_decomposition2"):
-        #     self.x_eig_vals2, self.x_eig2 = tf.self_adjoint_eig(self.patches_covariance2, name='x_eig')
-        #     self.top_x_eig2 = tf.reverse(self.x_eig2, axis=[2])[:, :, 0:l2]
-        #     self.top_x_eig2 = tf.transpose(tf.reshape(self.top_x_eig2, [1, k1, k2, l2]), [2, 1, 0, 3])
-        #
-        #     self.filt2_viz = tf.transpose(self.top_x_eig2, [3, 0, 1, 2])
-        #     tf.summary.image('filt2', self.filt2_viz, max_outputs=l2)
+        with tf.name_scope("extract_patches2"):
+            self.patches2 = tf.extract_image_patches(self.conv1_batch, [1, k1, k2, 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding='SAME', name='patches')
+            self.patches2 = tf.expand_dims(tf.reshape(self.patches2, [-1, k1 * k2], name='patches_shaped'), axis=2)
+            self.zero_mean_patches2 = self.patches2 - tf.reduce_mean(self.patches2, axis=1, keep_dims=True, name='patch_means')
+            x2 = tf.transpose(self.zero_mean_patches2, [2, 1, 0])
+            x2_trans = tf.transpose(self.zero_mean_patches2, [2, 0, 1])
+            self.patches_covariance2 = tf.matmul(x2, x2_trans, name='patch_covariance')
 
-        # with tf.name_scope("convolution2"):
-        #     self.conv2 = tf.nn.conv2d(self.conv1_batch, self.top_x_eig2, strides=[1, 1, 1, 1], padding='SAME')
-        #     self.conv2 = tf.reshape(tf.transpose(self.conv2, [0, 3, 1, 2]), [-1, l1, l2, info.IMAGE_W, info.IMAGE_H])
-        #     self.conv2_batch = tf.reshape(self.conv2, [-1, info.IMAGE_W, info.IMAGE_H, 1])
-        #     tf.summary.image('conv2', self.conv2_batch, max_outputs=l2)
+        with tf.name_scope("eignvalue_decomposition2"):
+            self.x_eig_vals2, self.x_eig2 = tf.self_adjoint_eig(self.patches_covariance2, name='x_eig')
+            self.top_x_eig2 = tf.reverse(self.x_eig2, axis=[2])[:, :, 0:l2]
+            self.top_x_eig2 = tf.transpose(tf.reshape(self.top_x_eig2, [1, k1, k2, l2]), [2, 1, 0, 3])
+
+            self.filt2_viz = tf.transpose(self.top_x_eig2, [3, 0, 1, 2])
+            tf.summary.image('filt2', self.filt2_viz, max_outputs=l2)
+
+        with tf.name_scope("convolution2"):
+            self.conv2 = tf.nn.conv2d(self.conv1_batch, self.top_x_eig2, strides=[1, 1, 1, 1], padding='SAME')
+            self.conv2_t = tf.reshape(tf.transpose(self.conv2, [0, 3, 1, 2]), [-1, l1, l2, info.IMAGE_W, info.IMAGE_H])
+            self.conv2_batch = tf.reshape(self.conv2_t, [-1, info.IMAGE_W, info.IMAGE_H, 1])
+            tf.summary.image('conv2', self.conv2_batch, max_outputs=l2)
 
         with tf.name_scope("h1"):
             self.h1_dim = 128
-            self.w_fc1 = tf.Variable(tf.truncated_normal([info.IMAGE_W * info.IMAGE_H * l1,  self.h1_dim], 0, 0.1), name='w_fc1')
+            self.w_fc1 = tf.Variable(tf.truncated_normal([info.IMAGE_W * info.IMAGE_H * l1 *l2,  self.h1_dim], 0, 0.1), name='w_fc1')
             self.b_fc1 = tf.Variable(tf.constant(0.1, shape=[self.h1_dim]), name='b_fc1')
-            self.conv1_flat = tf.reshape(self.conv1, [-1, info.IMAGE_W * info.IMAGE_H * l1])
-            self.h1 = tf.nn.relu(self.conv1_flat @ self.w_fc1 + self.b_fc1, name='h1')
+            self.conv_flat = tf.reshape(self.conv2, [-1, info.IMAGE_W * info.IMAGE_H * l1 * l2])
+            self.h1 = tf.nn.relu(self.conv_flat @ self.w_fc1 + self.b_fc1, name='h1')
 
         with tf.name_scope("h2"):
             self.h2_dim = info.NUM_CLASSES
